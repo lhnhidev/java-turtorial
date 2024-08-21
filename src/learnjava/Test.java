5532b96e3ed9b37a5b24d95305700e6e4e7b1f5a
Nhi-Le <lhnhi420@gmail.com>
2024-08-21 19:30:41 +0700

    done lab -4

--- src/learnjava/Test.java
+++ src/learnjava/Test.java
@@ -1,5 +1,27 @@
 package learnjava;
 
-public class Test {
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		Integer[] myArray = new Integer[n];
+		
+		for (int i = 0; i < n;  i++) {
+			myArray[i] = scanner.nextInt();
+		}
+		
+		Arrays.sort(myArray, Collections.reverseOrder());
+		System.out.println(Arrays.toString(myArray));
+		
+		scanner.close();
+	}
+	
 }
